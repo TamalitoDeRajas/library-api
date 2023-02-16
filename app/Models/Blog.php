0cc19030304c53618bfd0a6b41dff7e0bc174e02
Hector Contreras <20203tn079@utez.edu.mx>
2023-02-16 00:26:08 -0600

    initial commit

---
+++ app/Models/Blog.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+
+    protected $table = 'blogs';
+    protected $fillable=[
+        'id',
+        'title',
+        'content',
+        'created_at',
+        'updated_at'
+    ];
+    /*protected $hidden =['id'];*/
+
+    public function findBlogById($id)
+    {
+        return Blog:: find($id);
+    }
+}
